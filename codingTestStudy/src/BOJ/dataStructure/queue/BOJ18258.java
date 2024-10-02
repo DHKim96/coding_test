fd1edb7837ca695eabbcc0e144e496f6726e964c
DHKim96 <history8549@gmail.com>
2024-10-03 03:08:22 +0900

    큐 구현

--- codingTestStudy/src/BOJ/dataStructure/queue/BOJ18258.java
+++ codingTestStudy/src/BOJ/dataStructure/queue/BOJ18258.java
@@ -1,9 +1,13 @@
-package BOJ;
+package BOJ.dataStructure.queue;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main {
-    /*
+public class BOJ18258 {
+	  /*
     BOJ NO.18258 : 큐 2
     */
     static int[] queue;
