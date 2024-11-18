c6cfc1e75d376674dd058dc601d6dde72f816142
kimdonghyun <history8549@gmail.com>
2024-11-18 11:50:34 +0900

    문풀

--- codingTestStudy/src/BOJ/algorithm/sort/BOJ10814.java
+++ codingTestStudy/src/BOJ/algorithm/sort/BOJ10814.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Member;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BOJ10814 {
@@ -18,22 +20,22 @@ public class BOJ10814 {
 	 * 내장 정렬 메서드 사용 & 람다식으로 compare 사용해 조건부 정렬 구현
 	 */
 
-	static class Member {
-		int age;
-		String name;
-
-		public Member() {}
-
-		public Member(int age, String name) {
-			this.age = age;
-			this.name = name;
-		}
-
-		@Override
-		public String toString() {
-			return this.age + " " + this.name;
-		}
-	}
+//	static class Member {
+//		int age;
+//		String name;
+//
+//		public Member() {}
+//
+//		public Member(int age, String name) {
+//			this.age = age;
+//			this.name = name;
+//		}
+//
+//		@Override
+//		public String toString() {
+//			return this.age + " " + this.name;
+//		}
+//	}
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,28 +43,37 @@ public class BOJ10814 {
         
         int N = Integer.parseInt(br.readLine());
         
-        Member[] memberList = new Member[N]; // 객체 배열 사용
-        
+//        Member[] memberList = new Member[N]; // 객체 배열 사용
+
+		Map<Integer, String> map = new HashMap<>();
+
+
         //입력 받기
         for (int i = 0; i < N; i++) {
         	String[] input = br.readLine().split(" ");
         	int age = Integer.parseInt(input[0]);
         	String name = input[1];
+
+			map.put(age, map.getOrDefault(age, "") + " " + name);
         	
-        	memberList[i] = new Member(age, name);
+//        	memberList[i] = new Member(age, name);
         }
 
         // Arrays.sort 는 안정 정렬(stable sort)  따라서 별도의 처리없이 입력 순서 유지
-        Arrays.sort(memberList, (member1, member2) -> Integer.compare(member1.age, member2.age));
+//        Arrays.sort(memberList, (member1, member2) -> Integer.compare(member1.age, member2.age));
 
         // 결과 출력
         StringBuilder sb = new StringBuilder();
        
-        for (int i = 0; i < N; i++) {
-            sb.append(memberList[i]).append("\n");
-        }
+//        for (int i = 0; i < N; i++) {
+//            sb.append(memberList[i]).append("\n");
+//        }
 
-        bw.write(sb.toString());
+		for (int key : map.keySet()) {
+			System.out.println(map.get(key));
+		}
+
+//        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
---
+++ codingTestStudy/src/SSG_coding_test/week5/BOJ14500.java
@@ -0,0 +1,75 @@
+package SSG_coding_test.week5;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ14500 {
+    /*
+        * [BOJ] 테트로미노
+        *
+        *
+        * // 시작점 기준으로 테트로미노 놓기 / 회전
+        *
+        * straight  (시작점 r,c 일때)
+        *   => (12시) [r, c], [r - 1, c], [r - 2, c], [r - 3, c]
+        *   => (3시) [r, c], [r, c + 1], [r, c + 2], [r, c + 3]
+        *   => (6시) [r, c], [r + 1, c], [r + 2, c], [r + 3, c]
+        *   => (9시) [r, c], [r, c - 1], [r, c - 2], [r, c - 3]
+        *
+        * square
+        *   => (12시) (r,c), (r,c+1) (r-1,c) (r-1,c+1)
+        *   => (3시) (r,c), (r,c+1) (r+1,c) (r+1,c+1)
+        *   => (6시) (r,c), (r,c-1) (r+1,c) (r+1,c-1)
+        *   => (9시) (r,c), (r,c-1) (r-1,c) (r-1,c-1)
+        *
+        * skew
+        *   // 회전
+        *   => (12시) (r,c) (r-1,c) (r-2,c) (r-2,c-1)
+        *       => 대칭 (r,c) (r-1,c) (r-2,c) (r-2,c+1)
+        *   => (3시) (r,c) (r,c+1) (r,c+2) (r-1,c+2)
+        *       => 대칭 (r,c) (r,c+1) (r,c+2) (r+1,c+2)
+        *   => (6시) (r,c) (r+1,c) (r+2,c) (r+2,c+1)
+        *       => 대칭 (r,c) (r+1,c) (r+2,c) (r+2,c-1)
+        *   => (9시) (r,c) (r,c-1) (r,c-2) (r+1,c-2)
+        *       => 대칭 (r,c) (r,c-1) (r,c-2) (r-1,c-2)
+        *
+        *
+        *
+        * L
+        *   // 회전
+        *   => (12시) (r,c) (r-1, c), (r-1, c-1), (r-2, c-1)
+        *       =>
+        *   => (3시) (r,c) (r, c+1), (r-1, c+1), (r-2, c+2)
+        *
+        *   => (6시) (r,c) (r+1, c), (r+1, c+1), (r+2, c+1)
+        *
+        *   => (9시) (r,c) (r, c-1), (r+1, c-1), (r+2, c-2)
+        *
+        * T
+        *
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(stk.nextToken()); // 세로
+        int M = Integer.parseInt(stk.nextToken()); // 가로
+
+        int[][] paper = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            stk = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                paper[i][j] = Integer.parseInt(stk.nextToken());
+            }
+        }
+
+        bw.write("");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
